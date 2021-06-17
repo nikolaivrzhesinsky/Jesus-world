5afbb90242bbfc7b69451e78be0fe99bbe30bdd1
nikolaivrzhesinsky <63013424+nikolaivrzhesinsky@users.noreply.github.com>
2021-06-17 16:00:01 +0300

    добавление алгоритмов

---
+++ Учебные проекты/Бинарные деревья_2 Зотин Николай 4212/Бинарные деревья_2 Зотин Николай 4212/Бинарные деревья_2 Зотин Николай 4212.cpp
@@ -0,0 +1,358 @@
+﻿#include <iostream>
+#include<iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+struct Node {
+	int key;
+	Node* left;
+	Node* rihgt;
+};
+class tree {
+public:
+	tree() {//создание дерева
+		root = NULL;
+	}
+	void set_newnode();
+	void print_tree();
+	void delete_tree();
+	int counter = 0;
+	void show_counter();
+	void delete_i();
+	void straight_obhod();
+	void simetric_obhod();
+	void back_obhod();
+private:
+
+	void straight_obhod(Node* order);
+	void simetric_obhod(Node* order);
+	void back_obhod(Node* order);
+	void delete_i(int v_key, Node* order);
+	Node* serching(int v_key, Node* order);
+	Node* serching_of_min(Node* order);
+	void set_newnode(int v_key, Node* order);
+	void delete_tree(Node* order);
+	void show_counter(Node* order);
+	Node* root;//узел дерева
+	Node* before = NULL;//предок искомого элемента
+	Node* before_min = NULL;//предок элеммента для замены
+};
+void tree::straight_obhod() {
+	counter = 0;
+	straight_obhod(root);
+
+
+}
+void tree::simetric_obhod() {
+	counter = 0;
+	simetric_obhod(root);
+
+}
+void tree::back_obhod() {
+
+	back_obhod(root);
+
+}
+void tree::straight_obhod(Node* order) {
+	if (order != NULL) {
+		cout << order->key;
+		straight_obhod(order->left);
+		straight_obhod(order->rihgt);
+	}
+}
+void tree::simetric_obhod(Node* order) {
+	if (order != NULL) {
+
+		straight_obhod(order->left);
+		cout << order->key;
+		straight_obhod(order->rihgt);
+	}
+}
+void tree::back_obhod(Node* order) {
+	if (order != NULL) {
+
+		straight_obhod(order->left);
+		straight_obhod(order->rihgt);
+		cout << order->key;
+	}
+}
+Node* tree:: serching_of_min(Node* order) {
+	while (order->left != NULL) {
+		before_min = order;
+		return serching_of_min(order->left);
+	}
+	return order;
+}
+Node* tree::serching(int v_key, Node* order) {//функция поиска элемента по ключу
+	if (order != NULL) {
+		if (v_key == order->key) {
+			return order;
+		}
+		if (v_key < order->key) {
+			before = order;
+			return serching(v_key, order->left);
+		}
+		else {
+			before = order;
+			return serching(v_key, order->rihgt);
+
+		}
+	}
+	else return NULL;
+}
+void tree::delete_i() {
+	if (root == NULL) {
+		cout << " Дерево пустое\n";
+	}
+	else {
+		int m_key;
+		cout << " Введите ключ элемента, который нужно удалить.\n";
+		cin >> m_key;
+		delete_i(m_key, root);
+	}
+}
+void tree::delete_i(int v_key, Node* order) {//функция пок не готова
+	Node* node_to_delete=NULL;
+	node_to_delete = serching(v_key, order);//найдем место элемента с ключем в дереве если такой есть
+	if (before != NULL) {
+		cout << " Ключ родителя " << before->key << endl;
+	}
+	if (node_to_delete == NULL) {
+		cout << " Такого эдемента нет в дереве\n";
+	}
+	else {
+		if (node_to_delete->left == NULL && node_to_delete->rihgt == NULL) {//случай когда лист.
+			if (before->key < node_to_delete->key) {
+				delete node_to_delete;
+				before->rihgt = NULL;
+				cout << " Узел удален\n";
+			}
+			else {
+				delete node_to_delete;
+				before->left= NULL;
+				cout << " Узел удален\n";
+			}	
+		}
+		
+		else {
+			if (node_to_delete->left == NULL && node_to_delete->rihgt != NULL) {//случай, когда у узла есть правое поддерево
+				if (before->key < node_to_delete->key) {
+					before->rihgt = node_to_delete->rihgt;
+					delete node_to_delete;
+					cout << " Узел удален\n";
+				}
+				else {
+					before->left = node_to_delete->rihgt;
+					delete node_to_delete;
+
+					cout << " Узел удален\n";
+				}
+			}
+			else {
+				if (node_to_delete->left != NULL && node_to_delete->rihgt == NULL) {//случай, когда у узла есть левое поддерево
+					if (before->key < node_to_delete->key) {
+						before->rihgt = node_to_delete->left;
+						delete node_to_delete;
+						cout << " Узел удален\n";
+					}
+					else {
+						before->left = node_to_delete->left;
+						delete node_to_delete;
+
+						cout << " Узел удален\n";
+					}
+				}
+				else {
+					if (node_to_delete->left != NULL && node_to_delete->rihgt != NULL) {//случай, когда у узла есть 2 поддерева
+						Node* tmp = NULL;
+						Node* elem_min = serching_of_min(node_to_delete->rihgt);//поиск элемента для замены.
+						cout << "  Ключ элемнта для замены " << elem_min->key << endl;
+							node_to_delete->key = elem_min->key;
+							before_min->left = elem_min->rihgt;
+							delete elem_min;
+							cout << " Элемента удален\n";
+						
+						
+					}
+				}
+			}
+		}
+		
+	}
+	
+}
+void tree::show_counter() {
+	show_counter(root);
+	cout << " Количесво листьев в дереве-" << counter << endl;
+	counter = 0;
+}
+void tree::show_counter(Node* order) {
+	if (order != NULL) {
+		counter++;
+		show_counter(order->left);
+		show_counter(order->rihgt);
+	}
+
+
+}
+void tree::delete_tree() {
+	if (root == NULL) {
+		cout << "Дерево пустое, удалять нечего\n";
+	}
+	else {
+		delete_tree(root);
+		cout << " Элементы успешно удалены\n";
+		root = NULL;
+	}
+}
+void tree::delete_tree(Node* order) {
+	if (order != NULL) {
+		delete_tree(order->left);
+		delete_tree(order->rihgt);
+		delete order;
+	}
+
+}
+void tree::set_newnode(int v_key, Node* order) {
+	if (v_key < order->key) {
+		if (order->left != NULL) {
+			set_newnode(v_key, order->left);
+		}
+		else {
+			order->left = new Node;
+			order->left->key = v_key;
+			order->left->left = NULL;
+			order->left->rihgt = NULL;
+			cout << " Новый узел " << order->left->key << " создан\n";
+
+		}
+
+	}
+	else if (v_key >= order->key) {
+		if (order->rihgt != NULL) {
+			set_newnode(v_key, order->rihgt);
+		}
+		else {
+			order->rihgt = new Node;
+			order->rihgt->key = v_key;
+			order->rihgt->left = NULL;
+			order->rihgt->rihgt = NULL;
+			cout << " Новый узел " << order->rihgt->key << " создан\n";
+
+		}
+	}
+}
+void tree::set_newnode() {
+	int m_key;
+	cout << "\n Введите ключь нового узла -";
+	cin >> m_key;
+	if (root != NULL) {
+		set_newnode(m_key, root);
+	}
+	else {
+		root = new Node;
+		root->key = m_key;
+		root->left = NULL;
+		root->rihgt = NULL;
+
+		cout << " Новый узел " << root->key << " создан\n";
+
+	}
+}
+void tree::print_tree() {
+
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	tree element;
+	int exit = 1;
+	int operation;
+	while (exit == 1) {
+		cout << "\n\n\n           Меню выбора действий\n";
+		cout << "\n Выберите номер той команды, которую надо выполнить\n";
+		cout << "\n1.   Создание бинарного дерева. \n";
+		cout << "\n2.   Удалить бинарное дерево.\n";
+		cout << "\n3.   Добавить узел в бинарное дерево.\n";
+		cout << "\n4.   Удалить узел из бинарного дерева. \n";
+		cout << "\n5.   Вывод на экран содержимого дерева.\n";
+		cout << "\n6.   Количество листьев дерева.\n";
+		cout << "\n7.   Пhямой обход дереваю\n";
+		cout << "\n8.   Симетричный обход дерева\n";
+		cout << "\n9.   Обратный обход дерева\n";
+		cout << "\n0.  Выйти из программы\n";
+		operation = 0;
+		cin >> operation;
+		switch (operation) {
+		case 1:
+		{
+			system("cls");
+			cout << "         1.Пустое дерево создано.\n";
+			break;
+		}
+		case 2:
+		{
+			system("cls");
+			element.delete_tree();
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			cout << " Добавление нового узла\n";
+			element.set_newnode();
+
+			break;
+		}
+		case 4:
+		{
+			system("cls");
+			element.delete_i();
+			break;
+		}
+		case 6:
+		{
+			system("cls");
+			element.show_counter();
+			break;
+		}
+		case 7:
+		{
+			system("cls");
+			element.straight_obhod();
+			break;
+		}
+		case 8:
+		{
+			system("cls");
+			element.simetric_obhod();
+			break;
+		}
+		case 9:
+		{
+			system("cls");
+			element.back_obhod();
+			break;
+		}
+		case 0:
+		{
+			system("cls");
+			cout << "\nПроверка: Вы действительно хотите выйти из программы? Нажмите гужную цифру\n";
+			cout << "1-остаться в программе\n";
+			cout << "0-выйти из прогрпммы, прощайте\n";
+			cin >> exit;
+			system("cls");
+			break;
+		}
+		default:
+		{
+			system("cls");
+			cout << "\nВы ввели номер операции, которой в программе нет\n";
+			break;
+		}
+		}
+	}
+
+	return 0;
+}
+
---
+++ Учебные проекты/Курсовая. Жадные алгоритмы/Курсовая. Жадные алгоритмы/Курсовая. Жадные алгоритмы.cpp
@@ -0,0 +1,254 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void max_number() {
+	int zifra;
+	vector<int>set_massiv_of_numbers;
+	cout << "Заполните массив случайными цифрами (-1 это выход из заполнения)\n";//кнопка остановки -1
+	int i = 0;//счетчик для вектора
+	while (true) {
+		cin >> zifra;
+		if (zifra == -1) {
+			break;
+		}
+
+		if ((zifra >= 0) && (zifra < 10)) {
+			set_massiv_of_numbers.push_back(zifra);
+			i++;
+		}
+	}
+	system("cls");
+	cout << "Исходный массив: ";
+	for (auto const& element : set_massiv_of_numbers)
+		cout << element << ' ';
+	sort(set_massiv_of_numbers.begin(), set_massiv_of_numbers.end());  // сортировка
+	cout << "\n";
+	cout << "Наибольшее возможное число: ";
+	for (i = set_massiv_of_numbers.size() - 1; i >= 0; i--) {
+
+		cout << set_massiv_of_numbers.at(i);
+	}
+}
+void bag_pack() {
+	class Item {
+	private:
+		int weight;
+		int value;
+	public:
+		Item()
+		{
+			weight = value = 0;
+		}
+		/*Item(int weight, int value) {
+			this->weight = weight;
+			this->value = value;
+		}*/
+		void set_Item(int weight, int value) {
+			this->weight = weight;
+			this->value = value;
+		}
+		int get_weight() {
+			return weight;
+		}
+		int get_value() {
+			return value;
+		}
+		string get_info() {
+			return " Item: weight=" + to_string(weight) + " value=" + to_string(value);
+		}
+		double value_per_weight() {
+			return((double)value / weight);
+		}
+
+	};
+	const int Lenght = 3;
+	Item array[Lenght];
+	int weight, value;
+	cout << "Введите данные каждого предмета (вес, ценность)\n";
+	for (int i = 0; i < Lenght; i++) {
+		cin >> weight >> value;
+		array[i].set_Item(weight, value);
+	}
+	
+	system("cls");
+	cout << "Отсортировано по удельной полезности \n";
+	for (int i = 0; i < Lenght; i++) {
+		for (int j = 0; j < (Lenght - 1); j++) {
+			if (array[j].value_per_weight() < array[j + 1].value_per_weight()) {
+				std::swap(array[j], array[j + 1]);
+
+			}
+		}
+	}
+	for (int i = 0; i < Lenght; i++) {
+		cout << i + 1 << array[i].get_info() << endl;
+	}
+	cout << "Введтье вместимость рюкзака: ";
+	int Size_of_bag;
+	cin >> Size_of_bag;
+	int sum_weight = 0;
+	double sum_value = 0;
+	int current_Item=0;
+	while (current_Item < Lenght && sum_weight < Size_of_bag) {
+		if (sum_weight + array[current_Item].get_weight() < Size_of_bag) {
+			sum_value += array[current_Item].get_value();
+			sum_weight += array[current_Item].get_weight();
+		}
+		else {
+			sum_value += (((Size_of_bag - sum_weight) / (double)array[current_Item].get_weight()) * array[current_Item].get_value());
+			sum_weight = Size_of_bag;
+		}
+		current_Item++;
+	}
+	cout << "Наибольшее значение, которое мы можем собрать-" << sum_value << endl;
+}
+void algoritm_Deyckstr() {
+	int n, temp, start, temp_index, index;
+	cout << "Количество вершин в графе > ";
+	cin >> n;
+	int** GR;
+	GR = new int* [n];//создание двумерного массива для весов ребер нашего графа
+	for (int i = 0; i < n; i++) {
+		GR[i] = new int[n];     // инициализация 
+	}
+	for (int i = 0; i < n; i++)
+	{
+		GR[i][i] = 0;
+		for (int j = i + 1; j < n; j++) {
+			cout << "GR[" << i + 1 << "][" << j + 1 << "] > ";
+			cin >> temp;
+			GR[i][j] = temp;
+			GR[j][i] = temp;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << GR[i][j] << " ";
+		}
+		cout << endl;
+	}
+	start = 0;
+	int* route = new int[n];
+	bool* visit = new bool[n];
+	for (int i = 0; i < n; i++)
+	{
+		route[i] = INT_MAX; 
+		visit[i] = false;
+	}
+	route[start] = 0;
+	for (int count = 0; count < n; count++) {
+		int min = INT_MAX;
+		for (int i = 0; i < n; i++)
+			if (!visit[i] && route[i] <= min)//находим минимальную дистанцию до еще не пройденого этапа (локальный оптимум)
+			{
+				min = route[i]; 
+				index = i;
+			}
+		temp_index = index;
+		visit[temp_index] = true;//посетил новую точку с минимальной дистанцией от прошлой
+		for (int i = 0; i < n; i++) {
+			if (!visit[i] && GR[temp_index][i] && route[temp_index] != INT_MAX &&
+				route[temp_index] + GR[temp_index][i] < route[i]) {
+				route[i] = route[temp_index] + GR[temp_index][i];
+			}
+		}
+	}
+	cout << "Кратчайшие растояния из начальной точки до конечной\n";
+	for (int i = 0; i < n; i++) {
+		if (route[i] != INT_MAX)
+			cout << start+1 << " > " << i + 1 << " = " << route[i] << endl;
+		else cout << start + 1 << " > " << i + 1 << " = " << "маршрут недоступен" << endl;
+	}
+
+
+
+
+
+
+}
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "rus");
+	char tap_continue;
+	cout << "              Министерство образования и Науки Российской Федерации\n";
+	cout << "                      федеральное государственное бюджетное\n";
+	cout << "                  образовательное учреждение высшего образования\n";
+	cout << "           «Российский экономический университет имени  Г.В. Плеханова» \n";
+	cout << "\n\n           Институт математики, информационных систем и цифровой экономики \n";
+	cout << "\n\n  Базовая кафедра цифровой экономики института развития информационного общества \n";
+	cout << "\n\n\n\n\n                         КУРСОВАЯ РАБОТА\n";
+	cout << "\n\n\n      по дисциплине «Структуры и алгоритмы компьютерной обработки данных»\n";
+	cout << "\n\n\n                        на тему «Жадные алгоритмы\n";
+	cout << "\n\n\n                                                     Выполнил\n";
+	cout << "                                                     Студент группы 4112\n";
+	cout << "                                                     Очной формы обучения\n";
+	cout << "                                                        ИМИСиЦЭ\n";
+	cout << "                                                     Зотин Николай\n";
+	cout << "                                                     Научный руководитель:\n";
+	cout << "                                                     к.э.н., доцент Комлева Н.В..\n";
+	cout << "\n\n\n\n\n\n                           Москва 2021\n";
+	cout << "\n\n                            Нажмите любой символ  чтобы продолжить.\n";
+	cin >> tap_continue;
+	system("cls");
+	cout << "\n Надеюсь, что стерлось";
+	int exit = 1;
+	int operation;
+	while (exit == 1) {
+		cout << "\n\n\n           Меню выбора действий\n";
+		cout << "\n Выберите номер той команды, которую надо выполнить\n";
+		cout << "\n1.   Задача о максимальном числе\n";
+		cout << "\n2.   Задача о рюкзаке\n";
+		cout << "\n3.   Алгоритм Дейкстры\n";
+		cout << "\n0.   Выйти из программы\n";
+		operation = 0;
+		cin >> operation;
+		switch (operation) {
+		case 1:
+		{
+			system("cls");
+			max_number();
+			break;
+
+		}
+		case 2:
+		{
+			system("cls");
+			bag_pack();
+			break;
+		}
+		case 3:
+		{
+			system("cls");
+			algoritm_Deyckstr();
+			break;
+		}
+		case 0:
+		{
+			system("cls");
+			cout << "\nПроверка: Вы действительно хотите выйти из программы? Нажмите нужную цифру\n";
+			cout << "1-остаться в программе\n";
+			cout << "0-выйти из прогрпммы, прощайте\n";
+			cin >> exit;
+			system("cls");
+			break;
+		}
+		default:
+		{
+			system("cls");
+			cout << "\nВы ввели номер операции, которой в программе нет\n";
+			break;
+		}
+
+
+		}
+	}
+
+	return 0;
+}
